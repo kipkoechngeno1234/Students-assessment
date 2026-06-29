9a19bfcf9e427329786d9c5961e6db6a4fcbdf10
Finch Kyle <aron.ngenok@gmail.com>
2026-06-29 11:43:08 +0300

    Configure REST Framework

--- assessment_api/settings.py
+++ assessment_api/settings.py
@@ -39,7 +39,26 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'assessments',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'drf_spectacular',
+    'django_filters',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
 
 
 
