9da97d5147d5111ef1fb6a490828eb5cdd921b2b
Finch Kyle <aron.ngenok@gmail.com>
2026-06-29 13:13:30 +0300

    Configure JWT. Configure access and refresh token lifetimes

--- assessment_api/settings.py
+++ assessment_api/settings.py
@@ -59,7 +59,15 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+# JWT settings. Configure the access and refresh token lifetimes as needed.
+from datetime import timedelta
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+}
 
 
 MIDDLEWARE = [
