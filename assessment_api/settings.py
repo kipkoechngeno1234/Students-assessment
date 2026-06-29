c7abb0b0bc2594c73e2cac27acb4430aec02795b
Finch Kyle <aron.ngenok@gmail.com>
2026-06-29 11:09:17 +0300

    Student assessment system

--- assessment_api/settings.py
+++ assessment_api/settings.py
@@ -37,19 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
     'assessments',
-    'rest_framework',
-    'rest_framework_simplejwt',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -109,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-AUTH_USER_MODEL = 'assessments.CustomUser'
 
 
 # Internationalization
--- assessment_api/urls.py
+++ assessment_api/urls.py
@@ -15,20 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from rest_framework.routers import DefaultRouter
-from assessments.views import AssessmentViewSet, SubmissionViewSet
-from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-
-router = DefaultRouter()
-router.register(r'assessments', AssessmentViewSet)
-router.register(r'submissions', SubmissionViewSet)
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include('assessments.urls')),  # Include the assessments app URLs
 ]
