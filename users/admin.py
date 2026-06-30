72198d53622f181cb1ac18a0f6772abc57e9a4a2
Finch Kyle <aron.ngenok@gmail.com>
2026-06-30 18:27:21 +0300

    Register the User model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserManager
 
+from .models import User
 # Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = ("email", "role", "is_staff", "is_active")
+
+    ordering = ("email")
+    search_fields = ("email")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Role", {"fields": ("role")}),
+        ("Permission", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),   
+    )
+
+    add_fieldsets = (
+        (None, {"classes": ("wide",),
+       "fields": ("email", "password1", "password2", "role", "is_staff", "is_superuser" )}),
+    )
